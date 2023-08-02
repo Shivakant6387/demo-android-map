05d54e981a16d63e99cd17f99f26c1998126ca00
Shivakant6387 <shivakant5875@gmail.com>
2023-08-02 18:00:40 +0530

    map

---
+++ app/src/main/java/com/example/map4type/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.map4type;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap gMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        gMap=googleMap;
+
+        LatLng latLng=new LatLng(-34,151);
+        gMap.addMarker(new MarkerOptions().position(latLng).title("latlng"));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+}
